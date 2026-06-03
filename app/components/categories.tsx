c3e01a902eeb373cb48696e7640f00738b6ff5ae
ayushsoftechur <ayush.softechure@gmail.com>
2026-06-03 11:41:22 +0530

    change

--- app/components/categories.tsx
+++ app/components/categories.tsx
@@ -14,14 +14,15 @@ export default function Categories(){
             <h2 className="text-blue-300 text-4xl font-bold">Featured Categories</h2>
             <div className="flex gap-5">
             {Card.map(item=>(
-                <div key={item.heading} className="bg-white border border-gray-500 rounded-2xl">
+                <div key={item.heading} className="bg-white border border-gray-500 rounded-2xl py-20">
                     <div className="flex">
                         <div>
                             <Image src={item.img} alt="home" height={40} width={40}/>
                         </div>
                         <div>
                             <h5 className="text-2xl font-semibold">{item.heading}</h5>
-                            <p className="text-gray-500">{item.paragraph}</p>
+                            <p className="text-gray-500 mt-10">{item.paragraph}</p>
+                            <h1 className="text-3xl">ayush </h1>
                         </div>
                     </div>
                 </div>
